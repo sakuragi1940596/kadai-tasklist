54ad094ed211245b73f5feabb58aee352fd0cfa3
kazuhiro.kaita <sakuragi1940596@yahoo.co.jp>
2022-03-20 13:16:40 +0900

    getAllTasksを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,46 +7,44 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 //DBと接続、編集するためのDTOクラス作成
 @Entity
+//複数のデータをテーブルから取得するための特殊なSQL文であるJPQLを記述
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name="tasks")
 public class Task {
     //下記に、tasksテーブルに必要なカラムを追加するためのコードを記述
     //修飾子はすべてprivateでカプセル化して、getter,setterで変更できるようにする
+
     @Id
-    @Column(name="id")
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="title",length=255,nullable=false)
-    private String title;
-
-    @Column(name="content",length=255,nullable=false)
+    @Column(name = "content", length = 255, nullable = false)
     private String content;
 
-    @Column(name="created_at",nullable=false)
+    @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name="updated_at",nullable=false)
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
-    //getter,setter
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getContent() {
--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -4,10 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-//hibernateを利用してDBをDTOを介して操作するためのDAO作成
 public class DBUtil {
-
-    private static final String PERSISTENCE_UNIT_NAME="tasklist";
+    private static final String PERSISTENCE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
@@ -15,9 +13,10 @@ public class DBUtil {
     }
 
     private static EntityManagerFactory getEntityManagerFactory() {
-        if(emf==null) {
-            emf=Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        if(emf == null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         }
+
         return emf;
     }
 }
